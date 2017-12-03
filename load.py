79e44d295bf18fd96c4ad78ada129627de33637e
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2017-12-03 22:48:04 +0530

    Added location based event handling

--- load.py
+++ load.py
@@ -1,14 +1,21 @@
 from os.path import dirname, join
 from sys import platform
+import sys
 import time
 import ctypes
+import Tkinter as tk
+import myNotebook as nb
+from config import config
 
 CLIENT_ID = '386149818227097610'
+
+VERSION = '1.0.0'
 
 #
 # From discrod-rpc.h
 #
 discord_rpc = ctypes.cdll.LoadLibrary(join(dirname(__file__), platform == 'darwin' and 'libdiscord-rpc.dylib' or 'discord-rpc.dll'))
+
 
 class DiscordRichPresence(ctypes.Structure):
     _fields_ = [
@@ -16,18 +23,19 @@ class DiscordRichPresence(ctypes.Structure):
         ('details', ctypes.c_char_p),		# max 128 bytes
         ('startTimestamp', ctypes.c_int64),
         ('endTimestamp', ctypes.c_int64),
-        ('largeImageKey', ctypes.c_char_p),	# max 32 bytes
-        ('largeImageText', ctypes.c_char_p),	# max 128 bytes
-        ('smallImageKey', ctypes.c_char_p),	# max 32 bytes
-        ('smallImageText', ctypes.c_char_p),	# max 128 bytes
+        ('largeImageKey', ctypes.c_char_p),  # max 32 bytes
+        ('largeImageText', ctypes.c_char_p),  # max 128 bytes
+        ('smallImageKey', ctypes.c_char_p),  # max 32 bytes
+        ('smallImageText', ctypes.c_char_p),  # max 128 bytes
         ('partyId', ctypes.c_char_p),		# max 128 bytes
         ('partySize', ctypes.c_int),
         ('partyMax', ctypes.c_int),
-        ('matchSecret', ctypes.c_char_p),	# max 128 bytes
-        ('joinSecret', ctypes.c_char_p),	# max 128 bytes
-        ('spectateSecret', ctypes.c_char_p),	# max 128 bytes
+        ('matchSecret', ctypes.c_char_p),  # max 128 bytes
+        ('joinSecret', ctypes.c_char_p),  # max 128 bytes
+        ('spectateSecret', ctypes.c_char_p),  # max 128 bytes
         ('instance', ctypes.c_int8)
     ]
+
 
 class DiscordJoinRequest(ctypes.Structure):
     _fields_ = [
@@ -36,12 +44,14 @@ class DiscordJoinRequest(ctypes.Structure):
         ('avatar', ctypes.c_char_p)
     ]
 
+
 ReadyProc = ctypes.CFUNCTYPE(None)
-DisconnectedProc = ctypes.CFUNCTYPE(None, ctypes.c_int, ctypes.c_char_p)	# errorCode, message
+DisconnectedProc = ctypes.CFUNCTYPE(None, ctypes.c_int, ctypes.c_char_p)  # errorCode, message
 ErroredProc = ctypes.CFUNCTYPE(None, ctypes.c_int, ctypes.c_char_p)		# errorCode, message
 JoinGameProc = ctypes.CFUNCTYPE(None, ctypes.c_char_p)				# joinSecret
 SpectateGameProc = ctypes.CFUNCTYPE(None, ctypes.c_char_p)			# spectateSecret
 JoinRequestProc = ctypes.CFUNCTYPE(None, ctypes.POINTER(DiscordJoinRequest))
+
 
 class DiscordEventHandlers(ctypes.Structure):
     _fields_ = [
@@ -53,41 +63,46 @@ class DiscordEventHandlers(ctypes.Structure):
         ('joinRequest', JoinRequestProc)
     ]
 
+
 DISCORD_REPLY_NO, DISCORD_REPLY_YES, DISCORD_REPLY_IGNORE = range(3)
 
 Discord_Initialize = discord_rpc.Discord_Initialize
-Discord_Initialize.argtypes = [ctypes.c_char_p, ctypes.POINTER(DiscordEventHandlers), ctypes.c_int, ctypes.c_char_p]	# applicationId, handlers, autoRegister, optionalSteamId
+Discord_Initialize.argtypes = [ctypes.c_char_p, ctypes.POINTER(DiscordEventHandlers), ctypes.c_int, ctypes.c_char_p]  # applicationId, handlers, autoRegister, optionalSteamId
 Discord_Shutdown = discord_rpc.Discord_Shutdown
 Discord_Shutdown.argtypes = None
 Discord_UpdatePresence = discord_rpc.Discord_UpdatePresence
-Discord_UpdatePresence.argtypes = [ctypes.POINTER(DiscordRichPresence)];
+Discord_UpdatePresence.argtypes = [ctypes.POINTER(DiscordRichPresence)]
 Discord_Respond = discord_rpc.Discord_Respond
-Discord_Respond.argtypes = [ctypes.c_char_p, ctypes.c_int]	# userid, reply
+Discord_Respond.argtypes = [ctypes.c_char_p, ctypes.c_int]  # userid, reply
 
 #
 # Callback handlers
 #
 
+
 def ready():
     print 'ready'
+
 
 def disconnected(errorCode, message):
     print 'disconnected', errorCode, message
 
+
 def errored(errorCode, message):
     print 'errored', errorCode, message
+
 
 def joinGame(joinSecret):
     print 'joinGame', joinSecret
 
+
 def spectateGame(spectateSecret):
     print 'spectateGame', spectateSecret
+
 
 def joinRequest(request):
     print 'joinRequest', request.userId, request.username, request.avatar
 
-x = ReadyProc(ready)
-y = JoinRequestProc(joinRequest)
 
 event_handlers = DiscordEventHandlers(ReadyProc(ready),
                                       DisconnectedProc(disconnected),
@@ -98,34 +113,90 @@ event_handlers = DiscordEventHandlers(ReadyProc(ready),
 
 Discord_Initialize(CLIENT_ID, event_handlers, True, None)
 
+this = sys.modules[__name__]	# For holding module globals
 
-#
-# Update presence
-#
+this.presence_state = 'Connecting CMDR Interface'
+this.presence_details = ''
+this.time_start = time.time()
 
-presence = DiscordRichPresence('My state',
-                               'My details',
-                               0,
-                               0,
-                               'default',
-                               "Blade's Edge Arena",
-                               'rogue',
-                               'Rogue - Level 100',
-                               'Dunno',
-                               1,
-                               3,
-                               'secret1',
-                               'secret2',
-                               'secret3',
-                               0)
+def update_presence():
+    presence = DiscordRichPresence()
+    if config.getint("disable_presence")==0:
+        presence.state = this.presence_state
+        presence.details = this.presence_details
+    presence.startTimestamp = int(this.time_start)
+    Discord_UpdatePresence(presence)
+
+this.disablePresence = None
+
+
+def plugin_prefs(parent, cmdr, is_beta):
+    """
+    Return a TK Frame for adding to the EDMC settings dialog.
+    """
+    this.disablePresence = tk.IntVar(value=config.getint("disable_presence"))
+    frame = nb.Frame(parent)
+    nb.Checkbutton(frame, text="Disable Presence", variable=this.disablePresence).grid()
+    nb.Label(frame, text='Version %s' % VERSION).grid(padx=10, pady=10, sticky=tk.W)
+
+    return frame
+
+def prefs_changed(cmdr, is_beta):
+    """
+    Save settings.
+    """
+    config.set('disable_presence', this.disablePresence.get())
+    update_presence()
 
 def plugin_start():
-    presence.startTimestamp = int(time.time())
-    Discord_UpdatePresence(presence)
-    return 'Discord'
+    update_presence()
+    return 'DiscordPresence'
+
 
 def plugin_stop():
     Discord_Shutdown()
 
+
 def journal_entry(cmdr, is_beta, system, station, entry, state):
-    print entry['event']
+    if entry['event'] == 'StartUp':
+        this.presence_state = 'In %s' % system
+        if station is None:
+            this.presence_details = 'Flying in normal space'
+        else:
+            this.presence_details = 'Docked at %s' % station
+    elif entry['event'] == 'Location':
+        this.presence_state = 'In %s' % system
+        if station is None:
+            this.presence_details = 'Flying in normal space'
+        else:
+            this.presence_details = 'Docked at %s' % station
+    elif entry['event'] == 'StartJump':
+        this.presence_state = 'Jumping'
+        if entry['JumpType'] == 'Hyperspace':
+            this.presence_details = 'Jumping to %s' % entry['StarSystem']
+        elif entry['JumpType'] == 'Supercruise':
+            this.presence_details = 'Preparing for supercruise'
+    elif entry['event'] == 'SupercruiseEntry':
+        this.presence_state = 'In %s' % system
+        this.presence_details = 'Supercruising'
+    elif entry['event'] == 'SupercruiseExit':
+        this.presence_state = 'In %s' % system
+        this.presence_details = 'Flying in normal space'
+    elif entry['event'] == 'FSDJump':
+        this.presence_state = 'In %s' % system
+        this.presence_details = 'Supercruising'
+    elif entry['event'] == 'Docked':
+        this.presence_state = 'In %s' % system
+        this.presence_details = 'Docked at %s' % station
+    elif entry['event'] == 'Undocked':
+        this.presence_state = 'In %s' % system
+        this.presence_details = 'Flying in normal space'
+    elif entry['event'] == 'ShutDown':
+        this.presence_state = 'Connecting CMDR Interface'
+        this.presence_details = ''
+    elif entry['event'] == 'Music':
+        if entry['MusicTrack'] == 'MainMenu':
+            this.presence_state = 'Connecting CMDR Interface'
+            this.presence_details = ''
+    update_presence()
+            
